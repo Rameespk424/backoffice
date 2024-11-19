9b9384b4b2d6f565687246f36805bb8e5480c3c3
Ramees pk <83704042+Rameespk424@users.noreply.github.com>
2024-11-19 17:17:50 +0530

    updt: navbar style update

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    // Toggle active class on hamburger and nav-links
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+});
+
+
 
 // Fix Header on Scroll 
 
